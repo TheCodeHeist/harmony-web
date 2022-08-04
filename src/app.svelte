94be22264c3f4aa931de5a6c73a34f95d2ac4b1f
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2022-08-04 18:06:16 +0600

    From Scratch

--- src/app.svelte
+++ src/app.svelte
@@ -1,23 +1,45 @@
 <script lang="ts">
-  import { Router, Link, Route } from "svelte-navigator";
-  import Nav from "./lib/nav.svelte";
-  import About from "./routes/about.svelte";
-  import Show from "./routes/show.svelte";
-
-  const routes: Map<string, string> = new Map([
-    ["About", "/about"],
-    ["Show", "/"],
-  ]);
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
 </script>
 
-<Router>
-  <Nav {routes} />
+<main>
   <div>
-    <Route path="/">
-      <Show />
-    </Route>
-    <Route path="/about">
-      <About />
-    </Route>
+    <a href="https://vitejs.dev" target="_blank"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
   </div>
-</Router>
+  <h1>Vite + Svelt</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
